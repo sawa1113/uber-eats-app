f528b3011e49648bb32fe57a77823051cc8e54c3
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2026-02-04 16:58:05 +0900

    feat: set up frontend foundation and API integration

---
+++ frontend/src/containers/Restaurants.jsx
@@ -0,0 +1,20 @@
+import React, { Fragment, useEffect } from 'react';
+
+// apis
+import { fetchRestaurants } from '../apis/restaurants';
+
+export const Restaurants = () => {
+
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+
+  return (
+    <Fragment>
+      レストラン一覧
+    </Fragment>
+  )
+}

5861c38f7d08cd2be7035a72e6fc93c57af27561
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2026-02-05 17:39:47 +0900

    feat: implement food list page with modal, styling, and API integration

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,14 +1,157 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useReducer, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+
+
+// components
+import { LocalMallIcon } from '../components/Icons';
+import { FoodWrapper } from '../components/FoodWrapper';
+import { Skeleton } from '@mui/material';
+import { FoodOrderDialog } from '../components/FoodOrderDialog';
+
+// reducers
+import {
+  initialState as foodsInitialState,
+  foodsActionTyps,
+  foodsReducer,
+} from '../reducers/foods';
+
+// apis
+import { fetchFoods } from '../apis/foods';
+
+// images
+import MainLogo from '../images/logo.png';
+import FoodImage from '../images/food-image.jpg';
+
+
+// constants
+import { COLORS } from '../style_constants';
+import { REQUEST_STATE } from '../constants';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const FoodsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
+
+const submitOrder = () => {
+  // 後ほど仮注文のAPIを実装します
+  console.log('登録ボタンが押された！')
+}
 
 export const Foods = ({
   match
 }) => {
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
+  const initialState = {
+    isOpenOrderDialog: false,
+    selectedFood: null,
+    selectedFoodCount: 1,
+  }
+  const [state, setState] = useState(initialState);
+
+  useEffect(() => {
+    dispatch({ type: foodsActionTyps.FETCHING });
+    fetchFoods(match.params.restaurantsId)
+      .then((data) => {
+        dispatch({
+          type: foodsActionTyps.FETCH_SUCCESS,
+          payload: {
+            foods: data.foods
+          }
+        });
+      })
+  }, []);
+
   return (
     <Fragment>
-      フード一覧
-      <p>
-      restaurantsIdは {match.params.restaurantsId} です
-      </p>
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="/orders">
+            <ColoredBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodsList>
+        {
+          foodsState.fetchState === REQUEST_STATE.LOADING ?
+            <Fragment>
+              {
+                [...Array(12).keys()].map(i =>
+                  <ItemWrapper key={i}>
+                    <Skeleton key={i} variant="rect" width={450} height={180} />
+                  </ItemWrapper>
+                )
+              }
+            </Fragment>
+            :
+            foodsState.foodsList.map(food =>
+              <ItemWrapper key={food.id}>
+                <FoodWrapper
+                  food={food}
+                  // フードitemクリック時にsetStateする
+                  onClickFoodWrapper={(food) => setState({
+                    ...state,
+                    selectedFood: food,
+                    isOpenOrderDialog: true,
+                  })}
+                  imageUrl={FoodImage}
+                />
+              </ItemWrapper>
+            )
+        }
+      </FoodsList>
+      {
+        <FoodOrderDialog
+          isOpen={state.isOpenOrderDialog}
+          food={state.selectedFood}
+          countNumber={state.selectedFoodCount}
+          onClickCountUp={() => setState({
+            ...state,
+            selectedFoodCount: state.selectedFoodCount + 1,
+          })}
+          onClickCountDown={() => setState({
+            ...state,
+            selectedFoodCount: state.selectedFoodCount - 1,
+          })}
+          // 先ほど作った関数を渡します
+          onClickOrder={() => submitOrder()}
+          // モーダルを閉じる時はすべてのstateを初期化する
+          onClose={() => setState({
+            ...state,
+            isOpenOrderDialog: false,
+            selectedFood: null,
+            selectedFoodCount: 1,
+          })}
+        />
+      }
     </Fragment>
   )
 }
--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Link } from "react-router-dom";
 
 // components
-import Skeleton from '@material-ui/lab/Skeleton';
+import { Skeleton } from '@mui/material';
 
 // apis
 import { fetchRestaurants } from '../apis/restaurants';

7a18910117e87b971327114162a99ee28724f636
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2026-02-06 11:32:48 +0900

    feat: fetch draft order details from API

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,6 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
+
+import { fetchLineFoods } from '../apis/line_foods';
+
+import {
+  initialState,
+  lineFoodsActionTyps,
+  lineFoodsReducer,
+} from '../reducers/lineFoods';
 
 export const Orders = () => {
+
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+
+  useEffect(() => {
+    fetchLineFoods()
+      .then((data) =>
+        console.log(data)
+      )
+      .catch((e) => console.error(e));
+  }, []);
+
   return (
     <Fragment>
       注文画面

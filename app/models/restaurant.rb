eb3e67ed1b016931c7a15aea05a1d2337fdc810f
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2026-02-10 13:57:36 +0900

    マジックナンバーへ意味付け

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,8 +2,10 @@ class Restaurant < ApplicationRecord
   has_many :foods
   has_many :line_foods, through: :foods
 
+  DEFAULT_NAME_LENGTH = 30
+
   # 共通するバリデーション（presence: true）を1行にまとめる
-  validates :name, presence: true, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: DEFAULT_NAME_LENGTH }
   
   # 数値に関するバリデーション
   validates :fee, presence: true, numericality: { greater_than_or_equal_to: 0 }

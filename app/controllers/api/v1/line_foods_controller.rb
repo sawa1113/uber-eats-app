dece0ffff76516ad17032db1314ee1499bcd2707
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2026-02-10 14:05:43 +0900

    余計な配列処理を減らす

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -6,11 +6,21 @@ module Api
       def index
         line_foods = LineFood.active
         if line_foods.exists?
+          line_food_ids = []
+          count = 0
+          amount = 0
+
+          line_foods.each do |line_food|
+            line_food_ids << line_food.id # (1) idを参照して配列に追加する
+            count += line_food[:count] # (2)countのデータを合算する
+            amount += line_food.total_amount # (3)total_amountを合算する
+          end
+
           render json: {
-            line_food_ids: line_foods.map { |line_food| line_food.id },
+            line_food_ids: line_food_ids,
             restaurant: line_foods[0].restaurant,
-            count: line_foods.sum { |line_food| line_food[:count] },
-            amount: line_foods.sum { |line_food| line_food.total_amount },
+            count: count,
+            amount: amount,
           }, status: :ok
         else
           render json: {}, status: :no_content
@@ -20,16 +30,16 @@ module Api
       def create
         if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
           return render json: {
-            existing_restaurant: LineFood.other_restaurant(@ordered_food.restaurant.id).first.restaurant.name,
-            new_restaurant: Food.find(params[:food_id]).restaurant.name,
-          }, status: :not_acceptable
+                          existing_restaurant: LineFood.other_restaurant(@ordered_food.restaurant.id).first.restaurant.name,
+                          new_restaurant: Food.find(params[:food_id]).restaurant.name,
+                        }, status: :not_acceptable
         end
 
         set_line_food(@ordered_food)
 
         if @line_food.save
           render json: {
-            line_food: @line_food
+            line_food: @line_food,
           }, status: :created
         else
           render json: {}, status: :internal_server_error
@@ -45,12 +55,12 @@ module Api
 
         if @line_food.save
           render json: {
-            line_food: @line_food
+            line_food: @line_food,
           }, status: :created
         else
           render json: {}, status: :internal_server_error
         end
-      end      
+      end
 
       private
 
@@ -63,13 +73,13 @@ module Api
           @line_food = ordered_food.line_food
           @line_food.attributes = {
             count: ordered_food.line_food.count + params[:count],
-            active: true
+            active: true,
           }
         else
           @line_food = ordered_food.build_line_food(
             count: params[:count],
             restaurant: ordered_food.restaurant,
-            active: true
+            active: true,
           )
         end
       end

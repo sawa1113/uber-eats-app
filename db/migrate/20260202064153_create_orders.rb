dc8dcaa1c86901312d24fd634a438d9860754446
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2026-02-10 13:19:54 +0900

    カラムにコメント付与

--- db/migrate/20260202064153_create_orders.rb
+++ db/migrate/20260202064153_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
-      t.integer :total_price, null: false, default: 0 # 合計金額
+      t.integer :total_price, null: false, default: 0, comment: '合計金額'
 
       t.timestamps
     end
